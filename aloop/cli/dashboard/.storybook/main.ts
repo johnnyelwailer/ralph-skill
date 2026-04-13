8ac3eb63def9dc98205cd03b505cd929d0b2389b
Ubuntu <pj@vm-orb-shell.eb3vew4i0r4utgqklcjpxftlea.fx.internal.cloudapp.net>
2026-04-13 01:03:16 +0000

    feat: configure storybook 8 with react-vite and tailwind decorators

--- aloop/cli/dashboard/.storybook/main.ts
+++ aloop/cli/dashboard/.storybook/main.ts
@@ -1,11 +1,14 @@
 import { StorybookConfig } from '@storybook/react-vite';
 
-const config = {
+const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-themes'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
+  },
+  docs: {
+    autodocs: 'tag',
   },
 };
 
